2c4a7b7f1ae2a19b4fd6e362766226380fb39ef6
Dan Tkachuk <daniil.tkachuk@gmail.com>
2021-12-14 22:49:15 +0300

    2.1 Finished

---
+++ src/components/user.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import api from "../api";
+
+const User = () => {
+
+};
+
+export default User;
--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,37 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import api from "../api";
 
-const User = () => {
-  const users = api.users.fetchAll();
-  return (<tr>
-    <td>{users[0].name}</td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>);
-};
-
 const Users = () => {
-  console.log(api.users.fetchAll());
+  const [users, setUsers] = useState(api.users.fetchAll());
+  const badgeClass = "badge m-1 bg-";
+
+  const renderQualities = (user) => {
+    return user.qualities.map((quality, id) => (
+      <span key={id} className={badgeClass + quality.color}>{quality.name}</span>
+    ));
+  };
+
+  const handleUserDelete = (userIndex) => {
+    setUsers((prevState) =>
+      prevState.filter((user) => user !== users[userIndex])
+    );
+  };
+
+  const renderTotalUsers = (usersNum) => {
+    let peopleEndingStr = '';
+    let tusaEndingStr = 'ёт';
+
+    if ((usersNum < 11 || usersNum > 14 ) && (usersNum%10 > 1 && usersNum%10 < 5)) {
+      peopleEndingStr = 'а';
+      tusaEndingStr = 'ут';
+    }
+    if (usersNum === 0) {
+      return;
+    }
+    return (
+      <h2>
+        <span className='badge m-2 bg-primary'>
+          {usersNum} человек{peopleEndingStr} тусан{tusaEndingStr} с тобой сегодня
+        </span>
+      </h2>
+    )
+  };
+
+  const renderTable = (usersNum) => {
+    if (usersNum === 0) {
+      return (<h2>
+      <span className='badge m-2 bg-danger'>Никто не тусанёт с тобой сегодня</span>
+    </h2>);
+    } else {
+      return (
+        <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, ind) => (
+            <tr key={ind}>
+              <td>{user.name}</td>
+              <td>{renderQualities(user)}</td>
+              <td>{user.profession.name}</td>
+              <td>{user.completedMeetings}</td>
+              <td>{user.rate}/5</td>
+              <td>
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => handleUserDelete(ind)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      );
+    }
+  }
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Имя</th>
-          <th scope="col">Качества</th>
-          <th scope="col">Профессия</th>
-          <th scope="col">Встретился, раз</th>
-          <th scope="col">Оценка</th>
-          <th scope="col"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <User />
-        <User />
-      </tbody>
-    </table>
+    <>
+      {renderTotalUsers(users.length)}
+      {renderTable(users.length)}
+    </>
   );
 };
 
